2a540e033d78a7e8b59e7434e6e6233ce51db5d1
hameddjf <99538932+hameddjf@users.noreply.github.com>
2024-12-22 00:43:13 +0330

    final

--- Assemble/settings.py
+++ Assemble/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-w!y_ye8&vcaz%5^vd1ujq$__dqpbb6(jmd(m)&p6cmla77!69t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'hamednice.pythonanywhere.com']
 
 
 # Application definition
